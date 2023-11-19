f6b5c23717be90359836314dff72e37a30e92eb3
Le Long Truong Thinh <truongthinhkl1@gmail.com>
2023-11-19 19:03:46 +0700

    edit

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://youtube-gamma-eight.vercel.app/";
+export const BASE_URL = "http://youtube-gamma-eight.vercel.app/";
 
 const options = {
   params: {
